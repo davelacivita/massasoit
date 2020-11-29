68eb63a2e60e947224b7fb589e9a33a1c552c2a8
davelacivita <davelacivita@gmail.com>
2020-11-29 11:11:18 -0500

    updated ZipCode

--- intro_java/chpt-5/ZipCode_DL.java
+++ intro_java/chpt-5/ZipCode_DL.java
@@ -1,8 +1,14 @@
 import java.util.Scanner;
 public class ZipCode_DL {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         int d1, d2, d3, d4, d5;
+        boolean answer = true;
+        char response;
+
+        while (answer == true) {
+
         int zipOrg = getZip();
+        int zip = zipOrg;
 
         d1=zipOrg/10000;
         zipOrg= zipOrg%10000;
@@ -14,10 +20,28 @@ public class ZipCode_DL {
         zipOrg = zipOrg%10;
         d5 = zipOrg;
 
-        int checkDigit = getCheckDigit( d1,  d2,  d3,  d4,  d5);
-        System.out.println(checkDigit);
+        int checkDigit = getCheckDigit(d1,  d2,  d3,  d4,  d5);
+        System.out.printf("The bar code for the zip code %05d is: \n", zip );
+        printPipe();
+        printCode(d1);
+        printCode(d2);
+        printCode(d3);
+        printCode(d4);
+        printCode(d5);
+        printCode(checkDigit);
+        printPipe();
+        Scanner keyboard = new Scanner(System.in);
 
-
+        System.out.println("Would you like to enter another zip code? (y or n)");
+        response = (char)System.in.read();
+        if (response == 'y') {
+            answer = true;
+        }
+        else {
+            answer = false;
+            keyboard.close();
+        }
+        } // end while
     } //end main
 
     public static int getZip() {
@@ -27,30 +51,55 @@ public class ZipCode_DL {
             System.out.println("Please enter a zip code between 00000 and 99999.");
             input = keyboard.nextInt();
         }
-        while (input < 10000 || input > 99999);
-        keyboard.close();
+        while (input < 0 || input > 99999);
         return input;
     } // end getZip
 
-    // public static int[] getDigits(int zipOrg) {
-    //     int [] digits = new int[5];
-    //     digits[0]=zipOrg/10000;
-    //     zipOrg= zipOrg%10000;
-    //     digits[1]=zipOrg/1000;
-    //     zipOrg=zipOrg%1000;
-    //     digits[2]=zipOrg/100;
-    //     zipOrg=zipOrg%100;
-    //     digits[3]=zipOrg/10;
-    //     zipOrg = zipOrg%10;
-    //     digits[4] = zipOrg;
-    //     return digits;
-    // } //end getDigit
 
     public static int getCheckDigit(int d1, int d2, int d3, int d4, int d5) {
         int sum = d1+d2+d3+d4+d5;
-        int checkDigit = 10 -(sum%10);
+        int checkDigit = 10 - (sum%10);
         return checkDigit;
     } // end getCheckDigit
 
+    public static void printCode(int value) {
+        int digit = value;
+        switch (digit) {
+            case 1:
+            System.out.print(":::||");
+            break;
+            case 2:
+            System.out.print("::|:|");
+            break;
+            case 3:
+            System.out.print("::||:");
+            break;
+            case 4:
+            System.out.print(":|::|");
+            break;
+            case 5:
+            System.out.print(":|:|:");
+            break;
+            case 6:
+            System.out.print(":||::");
+            break;
+            case 7:
+            System.out.print("|:::|");
+            break;
+            case 8:
+            System.out.print("|::|:");
+            break;
+            case 9:
+            System.out.print("|:|::");
+            break;
+            default:
+            System.out.print("||:::");
+            break;
+        }
+    } // end printCode
+
+    public static void printPipe() {
+        System.out.print("|");
+    }
 
 } //end class
