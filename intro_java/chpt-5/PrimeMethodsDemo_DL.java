900e332348780b198cc8324987b73d72c42e405a
davelacivita <davelacivita@gmail.com>
2020-11-30 20:28:34 -0500

    updated Prime

--- intro_java/chpt-5/PrimeMethodsDemo_DL.java
+++ intro_java/chpt-5/PrimeMethodsDemo_DL.java
@@ -2,46 +2,34 @@ public class PrimeMethodsDemo_DL {
     public static void main(String[] args) {
         boolean result = true;
         boolean isPrime = true;
+
         String METHOD_PRIME = "Prime: ";
         String METHOD_SG = "Sophie Germain: ";
         String METHOD_TWIN = "Twin Prime: ";
         String METHOD_MP = "Mersenne Prime: ";
 
         int input = TypesOfPrimes_DL.getInput();
-        if (TypesOfPrimes_DL.determineIfPrime(input) == false) {
-            result = false;
-            System.out.println(METHOD_PRIME + result);
-        }
-        else {
-            System.out.println(METHOD_PRIME + result);
-        }
-        if (isPrime == true && result == true) {
-            if (TypesOfPrimes_DL.isSophieGermainPrime(input) == false) {
-                result = false;
-                System.out.println(METHOD_SG + result);
-            }
-            else {
-                System.out.println(METHOD_SG + result);
-            }
-            if (TypesOfPrimes_DL.isTwinPrime(input) == false) {
-                result = false;
-                System.out.println(METHOD_TWIN + result);
-            }
-            else {
-                System.out.println(METHOD_TWIN + result);
-            }
-            if (TypesOfPrimes_DL.isMersennePrime(input) == false) {
-                result = false;
-                System.out.println(METHOD_MP + result);
-            }
-            else {
-                System.out.println(METHOD_MP + result);
-            }
-        }// end if
-        else {
+        result = (TypesOfPrimes_DL.determineIfPrime(input));
+        isPrime = result;
+        System.out.println(METHOD_PRIME + result);
+
+        if (isPrime == true ) {
+            result = (TypesOfPrimes_DL.isSophieGermainPrime(input));
             System.out.println(METHOD_SG + result);
+            
+            result = (TypesOfPrimes_DL.isTwinPrime(input)); 
             System.out.println(METHOD_TWIN + result);
+            
+            result = (TypesOfPrimes_DL.isMersennePrime(input)); 
             System.out.println(METHOD_MP + result);
-        }
+            }//end if
+        else {
+            result = false;
+                System.out.println(METHOD_SG + result);
+                System.out.println(METHOD_TWIN + result);
+                System.out.println(METHOD_MP + result);
+        }//end else
+     
+        
     }// end main
 }// end class
